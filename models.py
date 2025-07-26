987a215bbb56505c4cc68a6235628542e20b560c
drusharma <karan.sharma1044186@gmail.com>
2025-07-27 03:25:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,14 +5,21 @@ db = SQLAlchemy()
 class Applicant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    dot = db.Column(db.String(50))
+    dot = db.Column(db.String(20))
     address = db.Column(db.String(200))
-    policies = db.relationship('Policy', backref='applicant', cascade='all, delete-orphan')
+    policies = db.relationship('Policy', backref='applicant', cascade="all, delete-orphan")
 
 class Policy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    applicants_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
-    bussiness_lines = db.Column(db.String(100))
+    applicant_id = db.Column(db.Integer, db.ForeignKey('applicant.id'))
+    business_lines = db.Column(db.String(100))
     policy_no = db.Column(db.String(100))
-    effective_date = db.Column(db.String(20))
-    expiration_date = db.Column(db.String(20))
+    effective_date = db.Column(db.Date)
+    expiration_date = db.Column(db.Date)
+    documents = db.relationship('PolicyDocument', backref='policy', cascade="all, delete-orphan")
+
+class PolicyDocument(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    policy_id = db.Column(db.Integer, db.ForeignKey('policy.id'))
+    document_name = db.Column(db.String(200))
+    document_path = db.Column(db.String(300))
